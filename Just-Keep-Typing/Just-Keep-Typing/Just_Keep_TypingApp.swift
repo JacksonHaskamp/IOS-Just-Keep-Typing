1b17a4b9c7f4da16f5d130c8a3fae357a149b819
Jackson Haskamp <jacksonhaskamp@gmail.com>
2022-05-01 14:39:44 -0500

    Initial Project Files

---
+++ Just-Keep-Typing/Just-Keep-Typing/Just_Keep_TypingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Just_Keep_TypingApp.swift
+//  Just-Keep-Typing
+//
+//  Created by Jackson on 5/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Just_Keep_TypingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
