9c799450c8e91ff360ee75e62d7a774a90bced0f
Jackson Haskamp <jacksonhaskamp@gmail.com>
2022-05-01 21:01:18 -0500

    made a basic textfield, not scalable to bigger canvas though

--- Just-Keep-Typing/Just-Keep-Typing/ContentView.swift
+++ Just-Keep-Typing/Just-Keep-Typing/ContentView.swift
@@ -7,13 +7,30 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var textString: String = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .center) {
+            Spacer()
+            TextField("Just Start Typing", text: $textString)
+                .onChange(of: textString) {
+                    print($0)
+                    //MARK: onTextChange, update lastChangedDate here
+                    
+                    
+                    
+                }
+                .frame(width: 50 ,height: 100, alignment: .center)
+                .padding()
+                
+            Spacer()
+        }
+    
+       
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
