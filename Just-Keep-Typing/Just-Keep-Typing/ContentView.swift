c3d9dccd72edede0c60e080b53ad673dddac1e39
Jackson Haskamp <jacksonhaskamp@gmail.com>
2022-05-02 17:19:10 -0500

    Timer, TextEditor Functionality
    added timer functionality and made it easy to continue working on TextEditor onChange functionality to change views when timer reaches threshold. Also added placeholder text that dissapears on tap.

--- Just-Keep-Typing/Just-Keep-Typing/ContentView.swift
+++ Just-Keep-Typing/Just-Keep-Typing/ContentView.swift
@@ -9,21 +9,40 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @State private var textString: String = ""
-    
+    @State private var textString: String = "Once, you start, don't stop."
+    @State var timer: Timer?
     var body: some View {
         VStack(alignment: .center) {
             Spacer()
-            TextField("Just Start Typing", text: $textString)
+            TextEditor(text: $textString)
                 .onChange(of: textString) {
                     print($0)
-                    //MARK: onTextChange, update lastChangedDate here
+                    //MARK: onTextChange, update lastChangedDate and check diffInSeconds here
+                    
+                    timer?.invalidate() // cancel any previous timers
+                    var counter = 0; // set counter to 0
+                    timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+                        print("counter is at \(counter)")
+                        if counter >= 5 {
+                            print("failView in timer")
+                            //MARK: switch views to fail view, and delete all text
+                            timer.invalidate()
+                        }
+                        
+                        counter = counter + 1
+                      
+                    }
                     
                     
                     
                 }
-                .frame(width: 50 ,height: 100, alignment: .center)
-                .padding()
+//                reference for this code: https://developer.apple.com/forums/thread/653108
+                .onTapGesture {
+                    if self.textString == "Once, you start, don't stop."{
+                        self.textString = ""
+                    }
+                }
+                
                 
             Spacer()
         }
